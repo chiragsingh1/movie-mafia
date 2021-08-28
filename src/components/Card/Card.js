3c00b7f1bf523cb9924088895a8b6a7a053b31c6
chiragsingh1 <imchirag.2717@gmail.com>
2021-08-28 21:29:15 +0530

    create badge from material-ui

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,3 +1,4 @@
+import { Badge } from "@material-ui/core";
 import { img_300, unavailable } from "../../config/config";
 
 import "./Card.css";
@@ -5,6 +6,10 @@ import "./Card.css";
 const Card = ({ id, poster, title, date, media_type, vote_average }) => {
   return (
     <div className="media">
+      <Badge
+        badgeContent={vote_average}
+        color={vote_average > 6 ? "primary" : "secondary"}
+      />
       <img
         className="poster"
         src={poster ? `${img_300}/${poster}` : unavailable}
