be2d096718f53388a7bf46debea987138b8f04c0
chiragsingh1 <imchirag.2717@gmail.com>
2021-08-29 14:12:42 +0530

    create TV page

--- src/components/Pages/Tv/Tv.js
+++ src/components/Pages/Tv/Tv.js
@@ -1,7 +1,64 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import useGenre from "../../../hooks/useGenre";
+import Card from "../../Card/Card";
+import CustomPagination from "../../CustomPagination/CustomPagination";
+import Genres from "../../Genres/Genres";
+
 const Tv = () => {
+  const [page, setPage] = useState(1);
+  const [content, setContent] = useState([]);
+  const [numOfPages, setNumOfPages] = useState();
+  const [genres, setGenres] = useState([]);
+  const [selectedGenres, setSelectedGenres] = useState([]);
+
+  const genreforURL = useGenre(selectedGenres);
+  // Fetching the movies from the api
+  const fetchMovies = async () => {
+    const url = `https://api.themoviedb.org/3/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=${page}&with_genres=${genreforURL}`;
+
+    const { data } = await axios.get(url);
+
+    console.log(data);
+
+    setContent(data.results);
+
+    setNumOfPages(data.total_pages);
+  };
+  // rendering the movies
+  useEffect(() => {
+    fetchMovies();
+    // eslint-disable-next-line
+  }, [page, genreforURL]);
+
   return (
     <div>
       <span className="pageTitle">TV 📺</span>
+      <Genres
+        type="tv"
+        genres={genres}
+        setGenres={setGenres}
+        selectedGenres={selectedGenres}
+        setSelectedGenres={setSelectedGenres}
+        setPage={setPage}
+      />
+      <div className="trending">
+        {content &&
+          content.map((c) => (
+            <Card
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              date={c.first_air_date || c.release_date}
+              media_type="tv"
+              vote_average={c.vote_average}
+            />
+          ))}
+      </div>
+      {numOfPages > 1 && (
+        <CustomPagination setPage={setPage} numOfPages={numOfPages} />
+      )}
     </div>
   );
 };
